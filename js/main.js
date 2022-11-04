fb17e7a8f65898b9b0422cce200f89355248d799
DarioRossi99 <rssdra99@gmail.com>
2022-11-04 17:59:43 +0100

    aggiustamento delle foto

--- js/main.js
+++ js/main.js
@@ -4,18 +4,29 @@ const sliderImgEl = document.querySelector(".sliderImg");
 const btnNext = document.getElementById("slider-btn-next");
 const btnPrev = document.getElementById("slider-btn-prev");
 
-let currentImgindex = 0
+let currentImgindex = 0;
 
 sliderImgEl.src = images[ currentImgindex ];
 
 btnNext.addEventListener("click", function() {
     console.log("Next btt click");
-
-    currentImgindex--
+    currentImgindex++;
+    const ultimoIndiceDisponibile = images.length - 1;
+    if ( currentImgindex > ultimoIndiceDisponibile){
+        currentImgindex = ultimoIndiceDisponibile;
+    }
+    sliderImgEl.src = images[ currentImgindex ];
 });
 
 btnPrev.addEventListener("click", function() {
     console.log("Prev btt click");
-
-    currentImgindex++
+    currentImgindex--;
+    if ( currentImgindex < 0 ){
+        currentImgindex = 0;
+    }
+    sliderImgEl.src = images[ currentImgindex ];
 });
+
+function ciao(){
+    alert("Belin");
+}
